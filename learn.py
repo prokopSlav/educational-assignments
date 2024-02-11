ddf1b33dc8a05225c7c4ec61fc2ff1ab7132b823
prokopSlav <146958587+prokopSlav@users.noreply.github.com>
2024-02-11 18:38:47 +0300

    Update learn.py

--- learn.py
+++ learn.py
@@ -62,3 +62,55 @@ class Money:
 
     def add_money(self, mn):
         self.__money += mn.__money
+
+
+"""Задача: реализовать два класса с координатами для создания прямоугольника."""
+
+class Point:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_coords(self):
+        return self.__x, self.__y
+
+
+class Rectangle:
+    def __init__(self, a, b, c=None, d=None):  # Как упростить инициализацию?
+        self.__sp = self.__ep = None
+        if isinstance(a, Point) and isinstance(b, Point):
+            self.__sp = a
+            self.__ep = b
+        elif all(map(lambda x: type(x) in (int, float), (a, b, c, d))):
+            self.__sp = Point(a, b)
+            self.__ep = Point(c, d)
+
+    def set_coords(self, sp, ep):
+        self.__sp = sp
+        self.__ep = ep
+
+    def get_coords(self):
+        return self.__sp, self.__ep
+
+    def draw(self):
+        print(f"Прямоугольник с координатами: {self.__sp} {self.__ep}")
+
+
+rect = Rectangle(0, 0, 20, 34)
+
+assert isinstance(rect, Rectangle) and hasattr(Rectangle, 'set_coords') and hasattr(Rectangle, 'get_coords') and hasattr(Rectangle, 'draw'), "в классе Rectangle присутствуют не все методы"
+
+r = Rectangle(1, 2.6, 3.3, 4)
+r.set_coords(Point(1, 2), Point(3, 4))
+sp, ep = r.get_coords()
+a, b = sp.get_coords()
+c, d = ep.get_coords()
+assert a == 1 and b == 2 and c == 3 and d == 4, "метод get_coords вернул неверные значения координат"
+
+r = Rectangle(Point(1, 2), Point(3, 4))
+sp, ep = r.get_coords()
+a, b = sp.get_coords()
+c, d = ep.get_coords()
+assert a == 1 and b == 2 and c == 3 and d == 4, "метод get_coords вернул неверные значения координат"
+
+assert isinstance(r._Rectangle__sp, Point) and isinstance(r._Rectangle__ep, Point), "атрибуты __sp и __ep должны ссылаться на объекты класса Point"
