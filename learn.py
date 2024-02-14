67ddf57e0d4a07311972b73d72f257800b2840d3
prokopSlav <146958587+prokopSlav@users.noreply.github.com>
2024-02-14 22:28:37 +0300

    Update learn.py

--- learn.py
+++ learn.py
@@ -244,6 +244,58 @@ print('Итоговый результат без использования pro
 print(f"Итоговый результат при использовании property:\n{test.__dict__}")
 
 
+"""Задание: реализовать два дескриптора для проверки добавленных товаров в корзину объекта Shop."""
+class StringValue:
+    def __init__(self, min_length=2, max_length=50, **kwargs):
+        self.min_length = kwargs['min_length'] = min_length
+        self.max_length = kwargs["max_length"] = max_length
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if isinstance(value, str) and (self.min_length <= len(value) <= self.max_length):
+            setattr(instance, self.name, value)
+
+
+class PriceValue:
+    def __init__(self, max_value=10000, **kwargs):
+        self.max_value = kwargs["max_value"] = max_value
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if isinstance(value, (int, float)) and (0 <= value <= self.max_value):
+            setattr(instance, self.name, value)
+
+
+class Product:
+    name = StringValue()
+    price = PriceValue()
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class SuperShop:
+    def __init__(self, name):
+        self.name = name
+        self.goods = list()
+
+    def add_product(self, product):
+        self.goods.append(product)
+
+    def remove_product(self, product):
+        self.goods.remove(product)
+
 
 
 
